a08d4847b74d5d6f41e79deca58cba9158ca9c08
Paul Cercueil <paul@crapouillou.net>
2020-01-24 10:57:49 -0300

    Blockcache: Check block hash if marked as outdated
    In many cases, when a block has been invalidated by a I/O write, the
    value written is the same as the previous value and the block doesn't
    really have to be recompiled. Detect these by calculating a hash of the
    memory area covered by the block and comparing the result with the hash
    computed when the block was first created.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- blockcache.c
+++ blockcache.c
@@ -167,5 +167,20 @@ u32 lightrec_calculate_block_hash(const struct block *block)
 
 bool lightrec_block_is_outdated(struct block *block)
 {
-	return !block->state->code_lut[lut_offset(block->pc)];
+	void **lut_entry = &block->state->code_lut[lut_offset(block->pc)];
+
+	if (*lut_entry)
+		return false;
+
+	if (block->hash == lightrec_calculate_block_hash(block)) {
+		/* The block was marked as outdated, but the content is still
+		 * the same */
+		if (block->function)
+			*lut_entry = block->function;
+		else
+			*lut_entry = block->state->get_next_block;
+		return false;
+	}
+
+	return true;
 }
